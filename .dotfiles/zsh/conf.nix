a4be4a61f46b030dd144ad1fb61200371c4d0e1d
Nadrieril <nadrieril@gmail.com>
2017-06-18 13:42:43 +0100

    Install diff-so-fancy through nix

--- .dotfiles/zsh/conf.nix
+++ .dotfiles/zsh/conf.nix
@@ -3,6 +3,7 @@ with import <nixpkgs> {};
 writeText "zshrc" ''
   export PATH="$PATH:${ag}/bin"
   export PATH="$PATH:${fzf.bin}/bin"
+  export PATH="$PATH:${gitAndTools.diff-so-fancy}/bin"
   source ${fzf.bin}/share/shell/completion.zsh
   source ${fzf.bin}/share/shell/key-bindings.zsh
 ''
