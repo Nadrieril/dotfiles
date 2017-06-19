6536794986cba6c47a7889acc00f2b6909ad57ef
Nadrieril <nadrieril@gmail.com>
2017-06-19 11:25:22 +0100

    [zsh] Install oh-my-zsh and its plugins via nix

--- .dotfiles/zsh/conf.nix
+++ .dotfiles/zsh/conf.nix
@@ -10,7 +10,16 @@ in
 
 writeText "zshrc" ''
   export PATH="${pkgs.lib.concatMapStrings (pkg: "${pkg}/bin:") userPackages}$PATH"
+
+  DISABLE_AUTO_UPDATE="true"
+  export ZSH=${oh-my-zsh}/share/oh-my-zsh
+  source $ZSH/oh-my-zsh.sh
+
   source ${fzf.bin}/share/shell/completion.zsh
   source ${fzf.bin}/share/shell/key-bindings.zsh
+
+  fpath+="${zsh-completions}/share/zsh/site-functions"
+  source ${zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+  source ${zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 ''
 
