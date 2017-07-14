67061b004fa5c3c1f7f498f200b9bddfb6bee11c
Nadrieril <nadrieril@gmail.com>
2017-07-14 12:04:36 +0100

    Fix nix wrapper

--- .config/nixpkgs/overlays/11-nix-wrapper.nix
+++ .config/nixpkgs/overlays/11-nix-wrapper.nix
@@ -1,11 +1,16 @@
 pkgs: super: {
   nix = pkgs.stdenv.mkDerivation {
     name = "nix-wrapped";
-    inherit (super.nix) version meta;
+    inherit (super.nix) version;
 
     nativeBuildInputs = [ pkgs.makeWrapper ];
 
     buildCommand = ''
+      mkdir -p $out
+      for item in ${super.nix}/*; do
+        ln -s $item $out/
+      done
+      rm $out/bin
       mkdir -p $out/bin
       for item in ${super.nix}/bin/*; do
         ln -s $item $out/bin/
@@ -13,6 +18,6 @@ pkgs: super: {
       wrapProgram $out/bin/nix-copy-closure --prefix PATH : ${pkgs.openssh}/bin
     '';
 
-    passthru = super.nix;
+    # passthru = super.nix;
   };
 }
