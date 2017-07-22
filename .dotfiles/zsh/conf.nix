038c866c4ea1b64b5efd2ae46050a9e38957cb5a
Nadrieril <nadrieril@gmail.com>
2017-07-22 20:14:54 +0100

    zsh: generate prompt statically via nix

--- .dotfiles/zsh/conf.nix
+++ .dotfiles/zsh/conf.nix
@@ -1,5 +1,55 @@
 with import <nixpkgs> {};
 
+let
+  prompt = let
+    intercalate = f: l:
+      lib.concatLists (
+        lib.zipListsWith (x: next_x:
+          [ x (f x next_x) ]
+        ) l (lib.tail l)
+      ) ++ lib.singleton (lib.last l);
+
+    separator = "";
+    default_fg = 15;
+    default_bg = 16;
+
+    write_color = c: "%{${c}%}";
+    write_segment = { text ? "", fg ? default_fg, bg ? default_bg, bold ? false }:
+      (if bold then write_color "[1m" else "") +
+      write_color "[38;5;${toString fg}m" +
+      write_color "[48;5;${toString bg}m" +
+      text +
+      write_color "[0m";
+
+    pwrl = segments:
+      let get_bg = s: if s?bg then s.bg else default_bg; in
+      lib.concatMapStrings write_segment (
+        intercalate (prev_s: next_s:
+          { fg=get_bg prev_s; bg=get_bg next_s; text=separator; }
+        ) segments
+      );
+
+    in ''
+      function git_prompt_info() {
+        ref=$(git symbolic-ref HEAD 2> /dev/null)
+        if [ $? -eq 0 ]; then
+          echo "${pwrl [ { bg=240; } { fg=250; bg=238; text=" ${"$"}{ref#refs/heads/} "; } {} ]}"
+        else
+          echo '${pwrl [ { bg=240; } {} ]}'
+        fi
+      }
+
+      PROMPT=$'\n'
+      PROMPT=$PROMPT'${pwrl [
+        {fg=250;bg=240;text=" %M ";}
+        {fg=250;bg=238;text=" %(!.%{'$fg[red]'%}.)%n ";}
+        {fg=15;bg=31;bold=true;text=" %~ ";}
+        {fg=250;bg=240;text=" %* ";}
+      ]}$(git_prompt_info)'$'\n'
+      PROMPT=$PROMPT'${pwrl [ { bg=236; text="%{%(?:'$fg_bold[green]':'$fg_bold[red]')%} $ ";} {} ]} '
+  '';
+
+in
 writeText "zshrc" ''
   DISABLE_AUTO_UPDATE="true"
   export ZSH=${oh-my-zsh}/share/oh-my-zsh
@@ -12,5 +62,7 @@ writeText "zshrc" ''
   fpath+="${nix-zsh-completions}/share/zsh/site-functions"
   source ${zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
   source ${zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+  ${prompt}
 ''
 
