580331b53b69ef7ff6fabc05d98c07d573273e14
Nadrieril <nadrieril@gmail.com>
2017-08-13 15:44:12 +0100

    Add desktop-specific packages

--- .dotfiles/userPackages.nix
+++ .dotfiles/userPackages.nix
@@ -4,6 +4,24 @@ with pkgs;
 let
   defaultEnvParams = (import <nixpkgs/nixos> { configuration={}; }).config.system.path;
 
+  hostname = builtins.getEnv "HOST";
+  is_desktop =
+    hostname == "meluin" ||
+    hostname == "limaew" ||
+    hostname == "nawlenca";
+
+  desktop_packages = [
+    rxvt_unicode
+    powerline-fonts
+    firefox
+    j4-dmenu-desktop
+    evince
+    xclip
+    spaceFM
+    xcape
+    mosh
+  ];
+
 in
 buildEnv {
   inherit (defaultEnvParams) ignoreCollisions postBuild;
@@ -21,9 +39,9 @@ buildEnv {
     gitAndTools.git-extras
     gnupg
     pass
-    xclip
-    rxvt_unicode
     beep
-  ];
+    jq
+    tmux
+  ] ++ lib.optionals is_desktop desktop_packages;
 }
 
